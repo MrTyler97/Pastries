025dd2a791718ad5bb194a4e53107fe6a4232f7c
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-10-06 16:57:45 -0400

    Finalizing model integration. Adding sheet component for classification.

--- Pastry Identifier/ContentView.swift
+++ Pastry Identifier/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CoreML
 import PhotosUI
+import Vision
 
 struct ContentView: View {
     // holds selected photo Item
@@ -16,11 +17,91 @@ struct ContentView: View {
     @State private var selectedImage: UIImage?
     // Camera sheet visability
     @State private var showingCamera = false
+    // Sheet viewability
+    @State private var showingSheet = false
+    // Classification Name
+    @State private var imageName = ""
+    // Classification note
+    @State private var note = ""
     
-    func classifyImage() {
+    // Function to classify pastry
+    func classifyImage(PastryImage:UIImage? ) {
         
-        // Initialize model
-//        let model = try PastryIdentifier(configuration: MLModelConfiguration())
+        guard let image = PastryImage else { return }
+        guard let ciImage = CIImage(image: image) else {
+            print("Failed to create CIImage")
+            return }
+        
+        do{
+            let config = MLModelConfiguration()
+            // Initialize model
+            let model = try Pastry_SelectorV5(configuration: config)
+            //
+            let visionModel = try VNCoreMLModel(for: model.model)
+            // Create a request
+            let request = VNCoreMLRequest(model: visionModel) { request, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                // Get results
+                guard let results = request.results as? [VNClassificationObservation], let topResult = results.first else { print("No results");return }
+                //
+                DispatchQueue.main.async{
+                    // Logic for classification diplayed
+                    if topResult.confidence > 0.80{
+                        self.imageName = topResult.identifier
+                        self.note = "High Confidence "
+                    }
+                    else if topResult.confidence > 0.60 && topResult.confidence < 0.80{
+                        self.imageName = topResult.identifier
+                        self.note = "Low Confidence, item may be misclassified"
+                    }
+                    else if topResult.identifier == "Other"{
+                        self.imageName = "Not Applicable"
+                        self.note = "This image does not fall within the classification types"
+                    }
+                    else {
+                        self.imageName = "Unclassified"
+                        self.note = "Very Low Confidence, try retaking the photo in a different orientation"
+                    }
+                }
+            }
+            let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+            try handler.perform([request])
+        } catch {
+            //
+            print("Error")
+        }
+    }
+    
+    // Sheet component view
+    struct SheetCompnent: View {
+        let imageName: String
+        let note: String
+        
+        var body: some View{
+            VStack{
+                // Place holder for asset image of item
+                Text("Placeholder")
+                    .foregroundStyle(.secondary)
+                    .padding()
+                    .frame(width: 200, height: 200)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(30)
+                    .padding()
+                // Heading for name of item (pullled from assets)
+                Text(imageName)
+                    .font(.headline)
+                Text(note)
+                // Information and history of item (pulled from assets
+                Text("Information")
+                    .padding()
+                // Nutrition component for item. (api call)
+                Text("Placeholder for nutrition component")
+            }
+        }
+        
     }
     
     var body: some View {
@@ -46,6 +127,10 @@ struct ContentView: View {
             Spacer()
             // Selector for classification
             if let selectedImage = selectedImage {
+                Button(action: {
+                    showingSheet.toggle()
+                    classifyImage(PastryImage: selectedImage)
+                }){
                     Text("Identify Pastry")
                         .font(.headline)
                         .foregroundStyle(Color(.white))
@@ -53,6 +138,12 @@ struct ContentView: View {
                         .frame(width: 300)
                         .background(Color.green.gradient)
                         .cornerRadius(30)
+                }
+                        .sheet(isPresented: $showingSheet){
+                            SheetCompnent(imageName: imageName, note: note)
+                            // Allows sheet to load halfway intially with the option to enlarge
+                                .presentationDetents([.medium, .large])
+                        }
             }
             Button(action: {
                 showingCamera.toggle()
